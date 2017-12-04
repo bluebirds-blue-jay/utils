7666f21b70802984e52359c3559bb65df65a53a3
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2017-12-04 11:24:55 -0800

    :rotating_light: Added tests for typesafe pluck/pick/omit.

---
+++ test/utils/pluck.test.ts
@@ -0,0 +1,18 @@
+import { pluck } from '../../src/utils/pluck';
+
+describe('.pluck()', function () {
+  it('should pluck from an array' +
+    '', async function () {
+    const source = [{
+      a: '1'
+    }, {
+      a: '2'
+    }];
+    const expected = [
+      '1',
+      '2'
+    ];
+    const actual = pluck(source, 'a');
+    expect(expected).to.eql(actual);
+  });
+});
