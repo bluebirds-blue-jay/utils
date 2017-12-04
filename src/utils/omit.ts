3ea9f344b5f9186d2f8a79bf794db16d4170b913
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2017-12-04 13:31:48 -0800

    :lipstick: Cleanup.

--- src/utils/omit.ts
+++ src/utils/omit.ts
@@ -4,5 +4,5 @@ export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in 
 export type Omit<T, K extends keyof T> = {[P in Diff<keyof T, K>]: T[P]};
 
 export function omit<T, K extends keyof T>(obj: T, keys: K|K[]): Omit<T, K> {
-  return Lodash.omit(obj, keys);
+  return <any>Lodash.omit(obj, keys);
 }
--- src/utils/pick.ts
+++ src/utils/pick.ts
@@ -1,5 +1,5 @@
 import * as Lodash from 'lodash';
 
 export function pick<T, K extends keyof T>(obj: T, keys: K | K[]): Pick<T, K> {
-  return Lodash.pick(obj, keys);
+  return <any>Lodash.pick(obj, keys);
 }
