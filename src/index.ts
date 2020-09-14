562da94ca6673d64b1596a82962b384c09eb018f
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2020-09-14 15:38:35 -0700

    :wrench: Moved index.ts to src to make it the same as bluejay/collection.

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { NOOP } from './constants/noop';
+export { TConstructible } from './types/constructible';
+export { DeepPartial } from './types/deep-partial';
+export { Diff } from './types/diff';
+export { makeArray } from './utils/make-array';
+export { pick } from './utils/pick';
+export { pluck } from './utils/pluck';
+export { omit } from './utils/omit';
+export { wait } from './utils/wait';
+export { clone } from './utils/clone';
+export { cloneDeep } from './utils/clone-deep';
+export { enumValues } from './utils/enum-values';
