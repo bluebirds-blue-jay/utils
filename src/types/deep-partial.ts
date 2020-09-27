acb8964b99990e7589769a91d94793379bb59c5f
Nicolas Gramlich <nicolas.gramlich@volvocars.com>
2020-09-27 00:45:15 -0700

    :wrench: Changed DeepPartial definition to show it now detects compile error.

--- src/types/deep-partial.ts
+++ src/types/deep-partial.ts
@@ -1,3 +1,5 @@
+type Value<T> = T extends AllowedPrimitives ? T : DeepPartial<T>;
+type AllowedPrimitives = boolean | string | number | Date;
 export type DeepPartial<T> = {
-  [P in keyof T]?: DeepPartial<T[P]>;
+  [P in keyof T]?:  T[P] extends (infer U)[] ? Value<U>[] : Value<T[P]>;
 };
