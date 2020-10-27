f036715904d88c616d742fa79fab7c7c6a35a3f5
Sylvain Estevez <sylvain@luxe.com>
2020-10-27 06:55:06 +0100

    :bug: Fixed type

--- src/utils/omit.ts
+++ src/utils/omit.ts
@@ -1,5 +1,5 @@
 import * as Lodash from 'lodash';
 
-export function omit<T extends object, K extends keyof T>(obj: T, keys: K|K[]): Omit<T, K> {
+export function omit<T extends {}, K extends keyof T>(obj: T, keys: K|K[]): Omit<T, K> {
   return Lodash.omit(obj, keys);
 }
